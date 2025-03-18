e4cb77e1d5563245b3644878cbc3a0656995ae5d
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2025-03-17 22:08:42 -0300

    sla

---
+++ ValidaPlaca/Database.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidaPlaca
+{
+    internal class Database
+    {
+        public void SalvarPlacaDeCarro(string placa)
+        {
+            string stringDeConexao = "server=localhost; port=3306 User ID=root; Password=root; database=ti_113_windowsforms";
+            MySqlConnection conexao = new MySqlConnection(stringDeConexao);
+            conexao.Open();
+
+            if (conexao.State == System.Data.ConnectionState.Open)
+                Console.WriteLine("Conectado ao Banco de Dados!");
+            else
+                Console.WriteLine("Não conectado");
+
+            conexao.Close();
+        }
+    }
+}
